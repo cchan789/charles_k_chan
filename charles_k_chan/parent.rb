3af6fae8361285996e4ccfe65c2bfa964c0d985e
Charles Chan <charleskingchan@rcn.com>
2016-06-17 14:12:32 +0000

    add hear_request method, add past_request hash, add driver code

--- charles_k_chan/parent.rb
+++ charles_k_chan/parent.rb
@@ -1,22 +1,47 @@
 class Parent
   @name
   @hoursofsleep
+  @past_requests
   
   attr_accessor :name
   attr_reader :hoursofsleep
   
   def initialize(name, hoursofsleep)
+    name = name.to_s
+    hoursofsleep = hoursofsleep.to_f
     @name = name
     @hoursofsleep = hoursofsleep
+    @past_requests = {}
+    # past requests will store request as key, and whether the request was granted as a boolean (true means yes, false means no)
   end
   
   def greet_child
     puts "*yaaaawn* I'm up, I'm up. #{@name} is up. What's the matter?"
   end
 
+  def hear_request(ask)
+    ask = ask.to_s
+    tarr = ask.split(' ')
+    rlen = tarr.length
+    if hoursofsleep >= rlen
+      puts "Okay, since you said #{ask} so nicely."
+      answer = true
+    else
+      puts "Not today, sweetie."
+      answer = false
+    end
+    @past_requests[ask] = answer
+    answer
+  end
+  
+  
 end
 
 joe = Parent.new('joe', 5)
 bob = Parent.new('bob', 6)
 mary = Parent.new('mary', 4)
 mary.greet_child
+tesarr = ["can i have dog","buy me car", "please can I have an jet", "could i possibly have access to a new phone", "I would very much like for your to acquire an antique clock for me"]
+tesarr.each do |testring|
+  bob.hear_request(testring)
+end
