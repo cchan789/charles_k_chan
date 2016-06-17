53af8fa821f7a5fe718bc20ffe4b59921282489d
Charles Chan <charleskingchan@rcn.com>
2016-06-17 13:53:08 +0000

    add greet_child method

--- charles_k_chan/parent.rb
+++ charles_k_chan/parent.rb
@@ -8,11 +8,15 @@ class Parent
   def initialize(name, hoursofsleep)
     @name = name
     @hoursofsleep = hoursofsleep
+  end
   
+  def greet_child
+    puts "*yaaaawn* I'm up, I'm up. #{@name} is up. What's the matter?"
   end
 
 end
 
-joe = Parent.new(joe, 5)
-bob = Parent.new(bob, 6)
-mary = Parent.new(mary, 4)
+joe = Parent.new('joe', 5)
+bob = Parent.new('bob', 6)
+mary = Parent.new('mary', 4)
+mary.greet_child
