a00809af6e6854154947833b2841b7486a01b3a4
Charles Chan <charleskingchan@rcn.com>
2016-06-17 13:45:51 +0000

    make parent class

---
+++ charles_k_chan/parent.rb
@@ -0,0 +1,15 @@
+class Parent
+  @name
+  @hoursofsleep
+  
+  def initialize(name, hoursofsleep)
+    @name = name
+    @hoursofsleep = hoursofsleep
+  
+  end
+
+end
+
+joe = Parent.new(joe, 5)
+bob = Parent.new(bob, 6)
+mary = Parent.new(mary, 4)
