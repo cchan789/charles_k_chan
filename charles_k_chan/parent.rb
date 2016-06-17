1ee5e8e03716cd9c635a3f9478a3d4fb5d7676b0
Charles Chan <charleskingchan@rcn.com>
2016-06-17 15:26:28 +0000

    make command line interface for make one parent and interacting with them (let user type in requests) add coffee method

--- charles_k_chan/parent.rb
+++ charles_k_chan/parent.rb
@@ -26,7 +26,7 @@ class Parent
     tarr = ask.split(' ')
     rlen = tarr.length
     if hoursofsleep >= rlen
-      puts "Okay, since you said #{ask} so nicely."
+      puts "Okay, since you said '#{ask}' so nicely."
       answer = true
     else
       puts "Not today, sweetie."
@@ -66,16 +66,68 @@ class Parent
   # for each index in the vowel count array, check if the value is larger than the value at the variable holding an index number, and if so, update the variable holding the index number to the current one
   # for each index in the vowel count array, check if the value is equal to the one at the varaible holding an index number, and if so, print the value at that index in the array holding all the words from the requests
   
+  def drinkcoffee(cups)
+    cups = cups.to_i
+    if cups <=0
+      puts "that's not helping me awake"
+    elsif cups <= 2
+      @hoursofsleep += cups
+    elsif cups <= 5
+      @hoursofsleep = 2 + ((cups - 2)/2)
+    else
+      @hoursofsleep = 3.5 + ((cups - 5)/3)
+    end
+    
+  end
   
   
 end
 
-joe = Parent.new('joe', 5)
-bob = Parent.new('bob', 6)
-mary = Parent.new('mary', 4)
-mary.greet_child
-tesarr = ["can i have dog","buy me car", "please can I have an jet", "could i possibly have access to a new phone", "I would very much like for your to acquire an antique clock for me"]
-tesarr.each do |testring|
-  bob.hear_request(testring)
+# joe = Parent.new('joe', 5)
+# bob = Parent.new('bob', 6)
+# mary = Parent.new('mary', 4)
+# mary.greet_child
+# tesarr = ["can i have dog","buy me car", "please can I have an jet", "could i possibly have access to a new phone", "I would very much like for your to acquire an antique clock for me"]
+# tesarr.each do |testring|
+#  bob.hear_request(testring)
+# end
+# bob.display_request_history
+
+puts "Please input the parent's name"
+name = gets.chomp.to_s
+puts "Please input the hours of sleep they got last night"
+hrsleep = gets.chomp.to_f
+par = Parent.new(name,hrsleep)
+par.greet_child
+failedreq = 0
+askarr = []
+while failedreq < 5
+  puts "So, what's the matter?"
+  repeat = false
+  ask = gets.chomp.to_s
+  
+  askarr.each do |ind|
+    if ind == ask
+      repeat = true
+    else
+    end
+  end
+  if ask.length == 0
+    puts "If you're not going to say anything, I'm going back to sleep"
+    # handling blank request by auto terminating, doesn't add blank requests to past requests
+    failedreq = 5
+  elsif repeat
+    puts "Now you're just repeating yourself, that's not helping your case"
+    # handle repeat by incrementing failed request counter faster, prevents adding repeated requests to the past_requests hash
+    failedreq += 2
+  else
+    askarr.push(ask)
+    a = par.hear_request(ask)
+    if !a
+      failedreq +=1
+    else
+    end
+  end
 end
-bob.display_request_history
+#par.display_request_history
+puts "looks like it's naptime!"
